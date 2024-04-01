0752941725283ceb8c69da4a59c8ac94c1fee1a6
Brian Yamabe <byamabe@The-40-Year-Developer.local>
2024-03-31 19:48:22 -0700

    Format login and registration forms

--- pages/login.vue
+++ pages/login.vue
@@ -1,22 +1,43 @@
 <template>
-  <div>
-    <h1>Log in to your account!</h1>
-    <form @submit.prevent="login()">
-      <div>
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model="credentials.email" />
+  <div class="mx-auto max-w-sm">
+    <h1 class="text-2xl font-bold mb-4">Log in to your account!</h1>
+    <form
+      @submit.prevent="login()"
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+    >
+      <div class="mb-4">
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          v-model="credentials.email"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
       </div>
-      <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="credentials.password" />
+      <div class="mb-4">
+        <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+          >Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          v-model="credentials.password"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
       </div>
-      <div>
-        <button type="submit">Submit</button>
+      <div class="flex items-center justify-between">
+        <button
+          type="submit"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
       </div>
     </form>
   </div>
 </template>
-
 <script setup>
 const credentials = reactive({
   email: "",
--- pages/register.vue
+++ pages/register.vue
@@ -1,33 +1,74 @@
 <template>
-  <div>
-    <h1>Create an account!</h1>
-    <form @submit.prevent="register()">
-      <div>
-        <label for="first-name">First Name</label>
-        <input type="text" id="first-name" v-model="credentials.firstname" />
+  <div class="mx-auto max-w-sm">
+    <h1 class="text-2xl font-bold mb-4">Create an account!</h1>
+    <form
+      @submit.prevent="register()"
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+    >
+      <div class="mb-4">
+        <label
+          for="first-name"
+          class="block text-gray-700 text-sm font-bold mb-2"
+          >First Name</label
+        >
+        <input
+          type="text"
+          id="first-name"
+          v-model="credentials.firstname"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
       </div>
-      <div>
-        <label for="surname">Surname</label>
-        <input type="text" id="surname" v-model="credentials.surname" />
+      <div class="mb-4">
+        <label for="surname" class="block text-gray-700 text-sm font-bold mb-2"
+          >Surname</label
+        >
+        <input
+          type="text"
+          id="surname"
+          v-model="credentials.surname"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
       </div>
-      <div>
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model="credentials.email" />
+      <div class="mb-4">
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          v-model="credentials.email"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
       </div>
-      <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="credentials.password" />
+      <div class="mb-4">
+        <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+          >Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          v-model="credentials.password"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
       </div>
-      <div>
-        <label for="password">Repeat Password</label>
+      <div class="mb-4">
+        <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+          >Repeat Password</label
+        >
         <input
           type="password"
           id="repeat-password"
           v-model="credentials.passwordRepeat"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
-      <div>
-        <button type="submit">Submit</button>
+      <div class="flex items-center justify-between">
+        <button
+          type="submit"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
       </div>
     </form>
   </div>
