ea24f070aab4c2144d8f01f9e0bcef7addf979a1
Brian Yamabe <byamabe@The-40-Year-Developer.local>
2024-03-31 19:53:37 -0700

    update nav bar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <div>
-      <button v-if="user" @click="logOut()">Log Out</button>
+  <div class="flex justify-between items-center bg-gray-800 p-4">
+    <div class="ml-auto">
+      <button
+        v-if="user"
+        @click="logOut()"
+        class="text-white hover:text-gray-300"
+      >
+        Log Out
+      </button>
     </div>
-    <div>
-      <nuxt-link v-if="!user" to="/login">Login</nuxt-link>
-    </div>
-    <div>
-      <nuxt-link v-if="!user" to="/register">Register</nuxt-link>
+    <div class="flex flex-row-reverse">
+      <div>
+        <nuxt-link
+          v-if="!user"
+          to="/login"
+          class="text-white hover:text-gray-300"
+          >Login</nuxt-link
+        >
+      </div>
+      <div>
+        <nuxt-link
+          v-if="!user"
+          to="/register"
+          class="text-white hover:text-gray-300"
+          >Register</nuxt-link
+        >
+      </div>
     </div>
   </div>
 </template>
